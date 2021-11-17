9c5350a902bf1c5c55b9e45bbe36dcb48ea7f049
Denis Emanuel Pop <pop.denis1902@gmail.com>
2021-11-18 00:55:53 +0200

    checkout page, checkout item component

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -17,3 +17,10 @@ export const selectCartItemsCount = createSelector(
   (cartItems) =>
     cartItems.reduce((accum, cartItem) => accum + cartItem.quantity, 0)
 );
+
+export const selectCartTotal = createSelector([selectCartItems], (cartItems) =>
+  cartItems.reduce(
+    (accum, cartItem) => accum + cartItem.quantity * cartItem.price,
+    0
+  )
+);
