3c7e53cf30fef3e234398aa0c38f28a7fae280d5
Denis Emanuel Pop <pop.denis1902@gmail.com>
2021-11-18 22:14:47 +0200

    created collections overview for items, normalized data for collection for better performance

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -6,3 +6,16 @@ export const selectCollections = createSelector(
   [selectShop],
   (shop) => shop.collections
 );
+
+export const selectCollectionsForPreview = createSelector(
+  [selectCollections],
+  (collections) => Object.keys(collections).map((key) => collections[key])
+  //get all the keys, map over the resulted array of keys and return the array of collections corresponding to every key
+);
+
+//collectionUrlParam = hats/womens/mens/jackets
+export const selectCollection = (collectionUrlParam) =>
+  createSelector(
+    [selectCollections],
+    (collections) => collections[collectionUrlParam]
+  );
